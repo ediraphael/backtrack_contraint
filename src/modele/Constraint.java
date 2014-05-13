855250a7a1edb2c0984a28ad3514ab5e2161d04b
mickael <mickael.fardilha@gmail.com>
2014-05-13 16:39:01 +0200

    ajout variable dans contraintes

--- src/modele/Constraint.java
+++ src/modele/Constraint.java
@@ -2,6 +2,24 @@ package modele;
 
 public class Constraint
 {
-
+	int firstVar;
+	int secondVar;
+	//TODO le type de contrainte
 	
+	public int getFirstVar() 
+	{
+		return firstVar;
+	}
+	public void setFirstVar(int firstVar) 
+	{
+		this.firstVar = firstVar;
+	}
+	public int getSecondVar() 
+	{
+		return secondVar;
+	}
+	public void setSecondVar(int secondVar) 
+	{
+		this.secondVar = secondVar;
+	}
 }
