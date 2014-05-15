fc7dd1e376eb70488163f68b9d5b905f2f10d0fb
ediraphael <raphael.pillie@gmail.com>
2014-05-15 10:18:21 +0200

    Ajout des contrainte private au variable de la class Constraint

--- src/modele/Constraint.java
+++ src/modele/Constraint.java
@@ -2,23 +2,27 @@ package modele;
 
 public class Constraint
 {
-	int firstVar;
-	int secondVar;
-	//TODO le type de contrainte
-	
-	public int getFirstVar() 
+	private int firstVar;
+	private int secondVar;
+
+	// TODO le type de contrainte
+
+	public int getFirstVar()
 	{
 		return firstVar;
 	}
-	public void setFirstVar(int firstVar) 
+
+	public void setFirstVar(int firstVar)
 	{
 		this.firstVar = firstVar;
 	}
-	public int getSecondVar() 
+
+	public int getSecondVar()
 	{
 		return secondVar;
 	}
-	public void setSecondVar(int secondVar) 
+
+	public void setSecondVar(int secondVar)
 	{
 		this.secondVar = secondVar;
 	}
