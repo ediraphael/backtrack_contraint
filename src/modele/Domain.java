c05ecf097728de932a99b46730e8c57fca2a9d30
ediraphael <raphael.pillie@gmail.com>
2014-05-15 10:11:39 +0200

    Implémentation de la class Domain

--- src/modele/Domain.java
+++ src/modele/Domain.java
@@ -2,5 +2,34 @@ package modele;
 
 public class Domain
 {
+	private int bottomBoundary;
+	private int upperBoundary;
+
+	public Domain(int bottomBoundary, int upperBoundary)
+	{
+		super();
+		this.bottomBoundary = bottomBoundary;
+		this.upperBoundary = upperBoundary;
+	}
+
+	public int getBottomBoundary()
+	{
+		return bottomBoundary;
+	}
+
+	public void setBottomBoundary(int bottomBoundary)
+	{
+		this.bottomBoundary = bottomBoundary;
+	}
+
+	public int getUpperBoundary()
+	{
+		return upperBoundary;
+	}
+
+	public void setUpperBoundary(int upperBoundary)
+	{
+		this.upperBoundary = upperBoundary;
+	}
 
 }
