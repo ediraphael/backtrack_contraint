aac3bc959c77c1adbe11a9aba866197c18d1cb79
ediraphael <raphael.pillie@gmail.com>
2014-05-15 10:33:22 +0200

    Implémentation d'une méthode execute pour l'énumération operator afin de calculer les boolean

--- src/modele/Operator.java
+++ src/modele/Operator.java
@@ -2,18 +2,59 @@ package modele;
 
 public enum Operator
 {
-	INFERIOR("<"),
-	SUPERIOR(">"),
-	EQUAL("="),
-	NOTEQUAL("!="),
-	INFERIOREQUAL("<="),
-	SUPERIOREQUAL(">=");
+	INFERIOR("<")
+	{
+		public boolean execute(Variable left, Variable right)
+		{
+			return left.getValeur() < right.getValeur();
+		}
+	},
+	SUPERIOR(">")
+	{
+		public boolean execute(Variable left, Variable right)
+		{
+			return left.getValeur() > right.getValeur();
+		}
+	},
+	EQUAL("=")
+	{
+		public boolean execute(Variable left, Variable right)
+		{
+			return left.getValeur() == right.getValeur();
+		}
+	},
+	NOTEQUAL("!=")
+	{
+		public boolean execute(Variable left, Variable right)
+		{
+			return left.getValeur() != right.getValeur();
+		}
+	},
+	INFERIOREQUAL("<=")
+	{
+		public boolean execute(Variable left, Variable right)
+		{
+			return left.getValeur() <= right.getValeur();
+		}
+	},
+	SUPERIOREQUAL(">=")
+	{
+		public boolean execute(Variable left, Variable right)
+		{
+			return left.getValeur() >= right.getValeur();
+		}
+	};
 
 	private String representation;
 
 	private Operator(String representation)
 	{
 		this.representation = representation;
+	}
+
+	public boolean execute(Variable left, Variable right)
+	{
+		return false;
 	}
 
 	public String getRepresentation()
