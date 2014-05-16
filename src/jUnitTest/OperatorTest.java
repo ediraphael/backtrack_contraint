06c5bf465b850b5917f5c650a5a903d488c45cce
ediraphael <raphael.pillie@gmail.com>
2014-05-16 18:32:18 +0200

    Mise en place de test le dernier fail

--- src/jUnitTest/OperatorTest.java
+++ src/jUnitTest/OperatorTest.java
@@ -1,9 +1,13 @@
 package jUnitTest;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 
+import modele.Domain;
+import modele.DomainBoundaryException;
 import modele.Operator;
 import modele.Variable;
+
+import org.junit.Test;
 
 public class OperatorTest
 {
@@ -15,7 +19,117 @@ public class OperatorTest
 	public void inferiorTest()
 	{
 		setOperator(Operator.INFERIOR);
+		Domain leftDomain;
+		Domain rightDomain;
+		// Case
+		// ---[---]-------------------
+		// ---------[----]------------
+		initLeftVariable("left", 0, 10);
+		initRightVariable("right", 15, 20);
+		leftDomain = initDomain(0, 10);
+		rightDomain = initDomain(15, 20);
 
+		reduceDomains();
+		assertEquals(leftDomain, leftVariable.getDomains().get(0));
+		assertEquals(rightDomain, rightVariable.getDomains().get(0));
+
+		// Case
+		// ---[---]-------------------
+		// -----[----]----------------
+		initLeftVariable("left", 0, 10);
+		initRightVariable("right", 5, 20);
+		leftDomain = initDomain(0, 10);
+		rightDomain = initDomain(5, 20);
+		reduceDomains();
+		assertEquals(leftDomain, leftVariable.getDomains().get(0));
+		assertEquals(rightDomain, rightVariable.getDomains().get(0));
+
+		// Case
+		// --------[---]--------------
+		// -----[---------]-----------
+		initLeftVariable("left", 5, 15);
+		initRightVariable("right", 0, 20);
+		leftDomain = initDomain(5, 15);
+		rightDomain = initDomain(6, 20);
+		reduceDomains();
+		assertEquals(leftDomain, leftVariable.getDomains().get(0));
+		assertEquals(rightDomain, rightVariable.getDomains().get(0));
+
+		// Case
+		// -----[---------]-----------
+		// --------[---]--------------
+		initLeftVariable("left", 0, 20);
+		initRightVariable("right", 5, 15);
+		leftDomain = initDomain(0, 14);
+		rightDomain = initDomain(5, 15);
+		reduceDomains();
+		assertEquals(leftDomain, leftVariable.getDomains().get(0));
+		assertEquals(rightDomain, rightVariable.getDomains().get(0));
+
+		// Case
+		// -----[---------]-----------
+		// --[---------]--------------
+		initLeftVariable("left", 0, 20);
+		initRightVariable("right", -5, 15);
+		leftDomain = initDomain(0, 14);
+		rightDomain = initDomain(1, 15);
+		reduceDomains();
+		assertEquals(leftDomain, leftVariable.getDomains().get(0));
+		assertEquals(rightDomain, rightVariable.getDomains().get(0));
+
+		// Case
+		// -----[---------]-----------
+		// -----[---------]-----------
+		initLeftVariable("left", 0, 10);
+		initRightVariable("right", 0, 10);
+		leftDomain = initDomain(0, 9);
+		rightDomain = initDomain(1, 10);
+		reduceDomains();
+		assertEquals(leftDomain, leftVariable.getDomains().get(0));
+		assertEquals(rightDomain, rightVariable.getDomains().get(0));
+
+		// Case
+		// ----------[----]-----------
+		// --[---]--------------------
+		initLeftVariable("left", 10, 15);
+		initRightVariable("right", 0, 5);
+
+		reduceDomains();
+		assertTrue(leftVariable.getDomains().isEmpty());
+		assertTrue(rightVariable.getDomains().isEmpty());
+	}
+
+	public void reduceDomains()
+	{
+		try
+		{
+			operator.reduceDomains(leftVariable, rightVariable);
+		} catch (DomainBoundaryException e)
+		{
+			fail("Error during reducing domain");
+		}
+	}
+
+	public void initLeftVariable(String name, int bottomBoundary, int upperBoundary)
+	{
+		this.leftVariable = new Variable(name, initDomain(bottomBoundary, upperBoundary));
+	}
+
+	public void initRightVariable(String name, int bottomBoundary, int upperBoundary)
+	{
+		this.rightVariable = new Variable(name, initDomain(bottomBoundary, upperBoundary));
+	}
+
+	public Domain initDomain(int bottomBoundary, int upperBoundary)
+	{
+		try
+		{
+			return new Domain(bottomBoundary, upperBoundary);
+		} catch (DomainBoundaryException e)
+		{
+			fail("Error creation domain");
+		}
+		return null;
 	}
 
 	public Operator getOperator()
