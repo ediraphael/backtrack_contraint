8214e56a7befd74134912a3a26f765cbdcaff3b7
ediraphael <raphael.pillie@gmail.com>
2014-05-16 17:35:12 +0200

    Début implémentation test

--- src/jUnitTest/OperatorTest.java
+++ src/jUnitTest/OperatorTest.java
@@ -1,10 +1,50 @@
 package jUnitTest;
 
+import org.junit.Test;
+
 import modele.Operator;
+import modele.Variable;
 
 public class OperatorTest
 {
 	private Operator operator;
-	
-	
+	private Variable leftVariable;
+	private Variable rightVariable;
+
+	@Test
+	public void inferiorTest()
+	{
+		setOperator(Operator.INFERIOR);
+
+	}
+
+	public Operator getOperator()
+	{
+		return operator;
+	}
+
+	public void setOperator(Operator operator)
+	{
+		this.operator = operator;
+	}
+
+	public Variable getLeftVariable()
+	{
+		return leftVariable;
+	}
+
+	public void setLeftVariable(Variable leftVariable)
+	{
+		this.leftVariable = leftVariable;
+	}
+
+	public Variable getRightVariable()
+	{
+		return rightVariable;
+	}
+
+	public void setRightVariable(Variable rightVariable)
+	{
+		this.rightVariable = rightVariable;
+	}
 }
