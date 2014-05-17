5d92d23c91843dedb0660861922c2edb75ce22b8
ediraphael <raphael.pillie@gmail.com>
2014-05-17 13:13:01 +0200

    Ajout d'une class de Test pour la class Domain

---
+++ src/jUnitTest/DomainTest.java
@@ -0,0 +1,52 @@
+package jUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import modele.Domain;
+import modele.DomainBoundaryException;
+
+public class DomainTest
+{
+	private Domain domain;
+
+	@Test
+	public void normalTest()
+	{
+		try
+		{
+			domain = new Domain(0, 5);
+			assertTrue(domain != null);
+		} catch (DomainBoundaryException e)
+		{
+			fail("Fail during domain creation!");
+		}
+	}
+
+	@Test
+	public void tinyTest()
+	{
+		try
+		{
+			domain = new Domain(5, 5);
+			assertTrue(domain != null);
+		} catch (DomainBoundaryException e)
+		{
+			fail("Fail during domain creation!");
+		}
+	}
+
+	@Test
+	public void reverseTest()
+	{
+		try
+		{
+			domain = new Domain(10, 5);
+			assertTrue(domain == null);
+		} catch (DomainBoundaryException e)
+		{
+			assertTrue(domain == null);
+		}
+	}
+}
