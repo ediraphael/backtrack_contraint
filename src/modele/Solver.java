7d083f21c40f3cd6044db677da92fe0c3793296c
ediraphael <raphael.pillie@gmail.com>
2014-05-17 13:20:13 +0200

    implémentation de la class Solver

--- src/modele/Solver.java
+++ src/modele/Solver.java
@@ -1,6 +1,36 @@
 package modele;
 
+import java.util.ArrayList;
+
 public class Solver
 {
+	private ArrayList<Variable> variableList;
+	private ArrayList<Constraint> constraintList;
+
+	public Solver(ArrayList<Variable> variableList, ArrayList<Constraint> constraintList)
+	{
+		this.variableList = variableList;
+		this.constraintList = constraintList;
+	}
+
+	public ArrayList<Variable> getVariableList()
+	{
+		return variableList;
+	}
+
+	public void setVariableList(ArrayList<Variable> variableList)
+	{
+		this.variableList = variableList;
+	}
+
+	public ArrayList<Constraint> getConstraintList()
+	{
+		return constraintList;
+	}
+
+	public void setConstraintList(ArrayList<Constraint> constraintList)
+	{
+		this.constraintList = constraintList;
+	}
 
 }
