09f4555f312e474d35b9985b81a03e2495b07657
ediraphael <raphael.pillie@gmail.com>
2014-05-20 15:44:48 +0200

    AJout du package exception

---
+++ src/exception/VariableValueException.java
@@ -0,0 +1,26 @@
+package exception;
+
+public class VariableValueException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public VariableValueException()
+	{
+		super();
+	}
+
+	public VariableValueException(String message)
+	{
+		super(message);
+	}
+
+	public VariableValueException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public VariableValueException(Throwable cause)
+	{
+		super(cause);
+	}
+}
