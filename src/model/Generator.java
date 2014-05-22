66d8fedd6d484f58f9694bb4521df496a2d4daa5
ediraphael <raphael.pillie@gmail.com>
2014-05-22 14:46:43 +0200

    Changement de generaor pour eviter qu'il tourne trop en rond

--- src/model/Generator.java
+++ src/model/Generator.java
@@ -27,108 +27,122 @@ public class Generator
 	public String generate()
 	{
 		String content = "";
-		Random random = new Random();
-		for (int i = 0; i < nbVariable; i++)
+		boolean retry = false;
+		do
 		{
-			int randomNumber = random.nextInt(maxValue - minValue + 1) + minValue;
-			int bottomBoundary = (randomNumber < (maxValue - minValue) / 2) ? randomNumber : randomNumber - ((maxValue - minValue) / 2);
-			int upperBoundary = (randomNumber > (maxValue - minValue) / 2) ? randomNumber : randomNumber + ((maxValue - minValue) / 2);
-			content += "var " + bottomBoundary + ".." + upperBoundary + ": var" + i + ";\n";
-			try
+			content = "";
+			Random random = new Random();
+			for (int i = 0; i < nbVariable; i++)
 			{
-				variableList.add(new Variable("var" + i, new Domain(bottomBoundary, upperBoundary)));
-			} catch (DomainBoundaryException e)
-			{
-				System.err.println("Error generation variable : " + e.getMessage());
-			}
-		}
-
-		for (int i = 0; i < nbConstraint; i++)
-		{
-			int leftVariable;
-			int rightVariable;
-			int operator;
-			boolean noNullDomain = true;
-			do
-			{
-				boolean incoherence = false;
-				do
-				{
-					leftVariable = random.nextInt(variableList.size());
-					rightVariable = random.nextInt(variableList.size());
-					while (rightVariable == leftVariable)
-					{
-						rightVariable = random.nextInt(variableList.size());
-					}
-					operator = random.nextInt(Operator.values().length);
-					if (Operator.SUPERIOR == Operator.values()[operator])
-					{
-						incoherence = incoherence || constraintList.contains(new Constraint(variableList.get(leftVariable), variableList.get(rightVariable), Operator.INFERIOR));
-						incoherence = incoherence || constraintList.contains(new Constraint(variableList.get(rightVariable), variableList.get(leftVariable), Operator.values()[operator]));
-					} else if (Operator.EQUAL == Operator.values()[operator])
-					{
-						incoherence = incoherence || constraintList.contains(new Constraint(variableList.get(leftVariable), variableList.get(rightVariable), Operator.NOTEQUAL));
-					} else if (Operator.NOTEQUAL == Operator.values()[operator])
-					{
-						incoherence = incoherence || constraintList.contains(new Constraint(variableList.get(leftVariable), variableList.get(rightVariable), Operator.EQUAL));
-					}
-				} while (incoherence);
-
+				int randomNumber = random.nextInt(maxValue - minValue + 1) + minValue;
+				int bottomBoundary = (randomNumber < (maxValue - minValue) / 2) ? randomNumber : randomNumber - ((maxValue - minValue) / 2);
+				int upperBoundary = (randomNumber > (maxValue - minValue) / 2) ? randomNumber : randomNumber + ((maxValue - minValue) / 2);
+				content += "var " + bottomBoundary + ".." + upperBoundary + ": var" + i + ";\n";
 				try
 				{
-					ArrayList<Variable> newVariableList = new ArrayList<Variable>();
-					for (Variable variable : this.getVariableList())
+					variableList.add(new Variable("var" + i, new Domain(bottomBoundary, upperBoundary)));
+				} catch (DomainBoundaryException e)
+				{
+					System.err.println("Error generation variable : " + e.getMessage());
+				}
+			}
+
+			for (int i = 0; i < nbConstraint && !retry; i++)
+			{
+				int leftVariable;
+				int rightVariable;
+				int operator;
+				boolean noNullDomain = true;
+				int cptContraintRetry = -1;
+				do
+				{
+					cptContraintRetry++;
+					boolean incoherence = false;
+					do
 					{
-						newVariableList.add((Variable) variable.clone());
-					}
-					ArrayList<Constraint> newConstraintList = new ArrayList<Constraint>();
-					for (Constraint constraint : this.getConstraintList())
-					{
-						Variable left = null;
-						Variable right = null;
-						for (Variable variable : newVariableList)
+						leftVariable = random.nextInt(variableList.size());
+						rightVariable = random.nextInt(variableList.size());
+						while (rightVariable == leftVariable)
 						{
-							if (constraint.getLeftVariable().equals(variable))
+							rightVariable = random.nextInt(variableList.size());
+						}
+						operator = random.nextInt(Operator.values().length);
+						if (Operator.SUPERIOR == Operator.values()[operator])
+						{
+							incoherence = incoherence || constraintList.contains(new Constraint(variableList.get(leftVariable), variableList.get(rightVariable), Operator.INFERIOR));
+							incoherence = incoherence || constraintList.contains(new Constraint(variableList.get(rightVariable), variableList.get(leftVariable), Operator.values()[operator]));
+						} else if (Operator.EQUAL == Operator.values()[operator])
+						{
+							incoherence = incoherence || constraintList.contains(new Constraint(variableList.get(leftVariable), variableList.get(rightVariable), Operator.NOTEQUAL));
+						} else if (Operator.NOTEQUAL == Operator.values()[operator])
+						{
+							incoherence = incoherence || constraintList.contains(new Constraint(variableList.get(leftVariable), variableList.get(rightVariable), Operator.EQUAL));
+						}
+					} while (incoherence);
+
+					try
+					{
+						ArrayList<Variable> newVariableList = new ArrayList<Variable>();
+						for (Variable variable : this.getVariableList())
+						{
+							newVariableList.add((Variable) variable.clone());
+						}
+						ArrayList<Constraint> newConstraintList = new ArrayList<Constraint>();
+						for (Constraint constraint : this.getConstraintList())
+						{
+							Variable left = null;
+							Variable right = null;
+							for (Variable variable : newVariableList)
 							{
-								left = variable;
-							} else if (constraint.getRightVariable().equals(variable))
+								if (constraint.getLeftVariable().equals(variable))
+								{
+									left = variable;
+								} else if (constraint.getRightVariable().equals(variable))
+								{
+									right = variable;
+								}
+							}
+
+							newConstraintList.add(new Constraint(left, right, constraint.getOperator()));
+						}
+						newConstraintList.add(new Constraint(newVariableList.get(leftVariable), newVariableList.get(rightVariable), Operator.values()[operator]));
+
+						boolean oneTrue = true;
+						while (oneTrue)
+						{
+							oneTrue = false;
+							for (Constraint constraint : newConstraintList)
 							{
-								right = variable;
+								oneTrue = constraint.reduceDomainVariables() || oneTrue;
 							}
 						}
-
-						newConstraintList.add(new Constraint(left, right, constraint.getOperator()));
-					}
-					newConstraintList.add(new Constraint(newVariableList.get(leftVariable), newVariableList.get(rightVariable), Operator.values()[operator]));
-
-					boolean oneTrue = true;
-					while (oneTrue)
-					{
-						oneTrue = false;
-						for (Constraint constraint : newConstraintList)
+						for (Variable variable : newVariableList)
 						{
-							oneTrue = constraint.reduceDomainVariables() || oneTrue;
+							noNullDomain = noNullDomain && !variable.getDomains().isEmpty();
 						}
-					}
-					for (Variable variable : newVariableList)
+					} catch (Exception e)
 					{
-						noNullDomain = noNullDomain && !variable.getDomains().isEmpty();
+						// TODO: handle exception
 					}
-				} catch (Exception e)
-				{
-					// TODO: handle exception
-				}
-			} while (!noNullDomain);
+					if (cptContraintRetry > 100)
+					{
+						retry = true;
+					}
+				} while (!noNullDomain && !retry);
 
-			content += "constraint " + variableList.get(leftVariable).getName() + " " + Operator.values()[operator] + " " + variableList.get(rightVariable).getName() + ";\n";
-			constraintList.add(new Constraint(variableList.get(leftVariable), variableList.get(rightVariable), Operator.values()[operator]));
-		}
-		content += "output [";
-		for (Variable variable : variableList)
-		{
-			content += "\n\"\\n" + variable.getName() + "=\",show(" + variable.getName() + "),";
-		}
-		content += "];";
+				content += "constraint " + variableList.get(leftVariable).getName() + " " + Operator.values()[operator] + " " + variableList.get(rightVariable).getName() + ";\n";
+				constraintList.add(new Constraint(variableList.get(leftVariable), variableList.get(rightVariable), Operator.values()[operator]));
+			}
+			if (!retry)
+			{
+				content += "output [";
+				for (Variable variable : variableList)
+				{
+					content += "\n\"\\n" + variable.getName() + "=\",show(" + variable.getName() + "),";
+				}
+				content += "];";
+			}
+		} while (retry);
 		return content;
 	}
 
